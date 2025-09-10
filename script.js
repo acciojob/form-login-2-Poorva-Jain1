8d1810b92da3c7451c0eac4b12d7fcba85352b22
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-09-10 22:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const form = document.getElementById("loginForm");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+	 const firstName = form.elements["First Name"].value;
+  const lastName = form.elements["Last Name"].value;
+  const phone = form.elements["Phone Number"].value;
+  const email = form.elements["Email ID"].value;
+	const message = 
+    `First Name: ${firstName} ` +
+    `Last Name: ${lastName} ` +
+    `Phone Number: ${phone} ` +
+    `Email ID: ${email}`;
+	alert(message);
+});
